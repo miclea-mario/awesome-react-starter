e35ec9db1a4c00684dec8c6d83450f23948d75d2
Mario <miclea.alexandru.mario@gmail.com>
2026-06-17 11:59:56 +0300

    Improved 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,29 @@
-import { Button } from '@components';
+import { Button } from '@components/ui/button';
+import { Card } from '@components/ui/card';
+import { Empty, EmptyDescription, EmptyHeader, EmptyTitle } from '@components/ui/empty';
+import { ArrowLeftIcon } from 'lucide-react';
+import Link from 'next/link';
 
 const Page = () => {
   return (
     <main className="cover flex min-h-screen flex-col items-center justify-center px-4 py-8">
-      <div className="my-8 flex w-full max-w-xl flex-col rounded-lg bg-white px-4 py-8 lg:px-12">
-        <div className="text-center">
-          <p className="my-4 text-lg font-semibold">
-            This page does not exist, has been moved or deleted from the website.
-          </p>
-          <p className="my-4 text-lg font-semibold">
-            Try to rewrite the link or press the button below.
-          </p>
-          <Button href="/" className="button full primary mt-2">
-            Back to the main page
+      <Card>
+        <Empty>
+          <EmptyHeader>
+            <EmptyTitle>404 - Not Found</EmptyTitle>
+            <EmptyDescription>
+              The page you&apos;re looking for doesn&apos;t exist. Try searching for what you need
+              below.
+            </EmptyDescription>
+          </EmptyHeader>
+
+          <Button variant="link" asChild className="text-muted-foreground" size="sm">
+            <Link href="/" className="flex items-center gap-2">
+              <ArrowLeftIcon /> Back to Home
+            </Link>
           </Button>
-        </div>
-      </div>
+        </Empty>
+      </Card>
     </main>
   );
 };
