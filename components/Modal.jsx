35b9db30e5bb7b875bd6cac4dd0408a361e24a1f
Mario <miclea.alexandru.mario@gmail.com>
2026-06-19 16:03:42 +0300

    Add radix support for modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,15 +1,26 @@
-import { Modal } from 'react-bootstrap';
+import { Dialog } from 'radix-ui';
 
-const Popup = ({ isOpen, hide, title, children, footer, modalProps }) => {
+const Modal = ({ isOpen, title, children, footer }) => {
   return (
-    <Modal backdrop="static" centered={true} show={isOpen} onHide={hide} {...modalProps}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>{children}</Modal.Body>
-      {footer && <Modal.Footer>{footer}</Modal.Footer>}
-    </Modal>
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 isolate z-50 bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0" />
+        <Dialog.Content className="fixed top-1/2 left-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 rounded-xl bg-white p-4 text-sm text-gray-500 ring-1 ring-primary/10 duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95">
+          <Dialog.Title className="font-sans text-base leading-none font-medium">
+            {title}
+          </Dialog.Title>
+          {children}
+          {footer}
+          <Dialog.Close asChild>
+            <button className="absolute top-2 right-2">
+              <i className="fas fa-times"></i>
+              <span className="sr-only">Close</span>
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
-export default Popup;
+export default Modal;
